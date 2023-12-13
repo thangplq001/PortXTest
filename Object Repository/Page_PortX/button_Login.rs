027e4600e30381f0068e142c4c0e42e3e7941949
ASUS <ASUS@Vivobook>
2023-12-13 09:34:24 +0700

    test

---
+++ Object Repository/Page_PortX/button_Login.rs
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Login</name>
+   <tag></tag>
+   <elementGuidId>bfe3a167-6a85-43e7-9883-cd61fd9e0de3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>button[name=&quot;Action&quot;]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@name='Action']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>b7380d89-7da7-42da-862f-f43c23e3ae25</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>submit</value>
+      <webElementGuid>69375a66-67a3-4d3e-97f1-48e249b74c79</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>mb-3 btn btn-primary</value>
+      <webElementGuid>1110c86b-214c-402e-aad4-a1037caa2be5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>Action</value>
+      <webElementGuid>eef03650-c52d-4cee-b5bf-2aa92b425a62</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>value</name>
+      <type>Main</type>
+      <value>Login</value>
+      <webElementGuid>c848bdc3-f55f-4cd8-82f9-ef193dc11138</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-busy-text</name>
+      <type>Main</type>
+      <value>Processing...</value>
+      <webElementGuid>18648868-815b-46b6-a4cb-9b680d37c83e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                    
+                    Login
+                </value>
+      <webElementGuid>3b9049ba-9680-4256-8228-7e6009f4a123</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;abp-account-layout lpx-theme-light&quot;]/div[@class=&quot;container-fluid p-0 overflow-hidden&quot;]/div[@class=&quot;lpx-login-area&quot;]/div[@class=&quot;lpx-login-bg&quot;]/div[@class=&quot;d-flex flex-column justify-content-center min-vh-100&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-xxl-5 col-lg-7 col-md-8 col-11 mx-auto position-relative py-4&quot;]/div[@class=&quot;card mx-auto&quot;]/div[@class=&quot;card-body p-3 p-sm-4&quot;]/div[@class=&quot;account-module-form&quot;]/form[1]/div[@class=&quot;d-grid gap-2&quot;]/button[@class=&quot;mb-3 btn btn-primary&quot;]</value>
+      <webElementGuid>939f0235-4857-47c3-b39d-91a7fc0ccc61</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//button[@name='Action']</value>
+      <webElementGuid>dbd0851d-845c-467b-ad1f-3624cd954ea2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/button</value>
+      <webElementGuid>ff44f47b-e134-4202-99c9-27a34fde164c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[@type = 'submit' and @name = 'Action' and (text() = '
+                    
+                    Login
+                ' or . = '
+                    
+                    Login
+                ')]</value>
+      <webElementGuid>08793c9d-54d0-48d4-9b6f-358bbb0362e9</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
